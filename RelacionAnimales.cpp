590d312beaa4a40a3c36b1f87034e71541754905
Rodrovan <riramirez@uamv.edu.ni>
2023-10-29 22:21:08 -0600

    a

--- RelacionAnimales.cpp
+++ RelacionAnimales.cpp
@@ -10,8 +10,10 @@ struct Animal {
     string nombre;
     double energia;
     string tipo;
+    int semanasParaReproducir;  // Número de semanas requeridas para la reproducción
+    int semanasPasadas;  // Número de semanas pasadas desde la última reproducción
 
-    Animal(string _nombre, double _energia, string _tipo) : nombre(_nombre), energia(_energia), tipo(_tipo) {}
+    Animal(string _nombre, double _energia, string _tipo, int _semanasParaReproducir) : nombre(_nombre), energia(_energia), tipo(_tipo), semanasParaReproducir(_semanasParaReproducir), semanasPasadas(0) {}
 };
 
 bool Probabilidad(double prob) {
@@ -25,6 +27,17 @@ void Simular(vector<Animal>& animales) {
         for (size_t j = 0; j < animales.size(); j++) {
             animales[j].energia -= 1;
             cout << animales[j].nombre << " perdio 1 de energia." << endl;
+
+            // Verificar si el animal puede reproducirse
+            if (animales[j].semanasPasadas >= animales[j].semanasParaReproducir) {
+                string nuevoNombre = animales[j].nombre + "Hijo";  // Nuevo nombre para el hijo
+                Animal hijo(nuevoNombre, 50.0, animales[j].tipo, animales[j].semanasParaReproducir);  // Crear el nuevo animal hijo
+                animales.push_back(hijo);  // Agregar el hijo a la lista de animales
+                cout << animales[j].nombre << " se reprodujo y nacio " << nuevoNombre << "." << endl;
+                animales[j].semanasPasadas = 0;  // Reiniciar las semanas pasadas
+            } else {
+                animales[j].semanasPasadas++;  // Incrementar las semanas pasadas
+            }
         }
 
         for (size_t j = 0; j < animales.size(); j++) {
@@ -47,10 +60,10 @@ void Simular(vector<Animal>& animales) {
 int main() {
     srand(static_cast<unsigned>(time(0))); // Inicializa la semilla para números aleatorios
     vector<Animal> animales;
-    animales.push_back(Animal("Puma1", 50.0, "Puma"));
-    animales.push_back(Animal("Venado1", 50.0, "Venado"));
-    animales.push_back(Animal("Lobo1", 50.0, "Lobo"));
-    animales.push_back(Animal("Cuervo1", 50.0, "Cuervo"));
+    animales.push_back(Animal("Puma1", 50.0, "Puma", 5));  // Se requieren 5 semanas para la reproducción
+    animales.push_back(Animal("Venado1", 50.0, "Venado", 3));  // Se requieren 3 semanas para la reproducción
+    animales.push_back(Animal("Lobo1", 50.0, "Lobo", 4));  // Se requieren 4 semanas para la reproducción
+    animales.push_back(Animal("Cuervo1", 50.0, "Cuervo", 2));  // Se requieren 2 semanas para la reproducción
 
     Simular(animales);
 
